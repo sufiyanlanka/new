fa0c126e76d7f04ad28ec942edda55ed15c05129
sufiyanlanka <44215255+sufiyanlanka@users.noreply.github.com>
2021-04-10 13:20:10 -0700

    added python

---
+++ GitTest.py
@@ -0,0 +1,8 @@
+def rankchecker(profilescore):
+    profilescore=int(profilescore)
+    constant=0
+    for status in range(1,100):
+        lowrange=constant
+        constant=constant+40+(status*10)
+        if lowrange<= profilescore <=constant and round(profilescore,1)==profilescore:
+            return status

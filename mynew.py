4a2e243c0bcf21e8daf62a26eb1c7ec014fa3efc
sufiyanlanka <sufiyanlanka1997@gmail.com>
2021-04-11 13:20:40 -0700

    added login

--- mynew.py
+++ mynew.py
@@ -1,3 +1,17 @@
+def rankchecker(profilescore):
+    profilescore=int(profilescore)
+    constant=0
+    for status in range(1,100):
+        lowrange=constant
+        constant=constant+40+(status*10)
+        if lowrange<= profilescore <=constant and round(profilescore,1)==profilescore:
+            return status
+
+
+def Signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POS
+
 
 def login(request):
     if request.method == 'POST':
